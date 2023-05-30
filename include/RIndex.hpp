d0ee7b5f99471c9cf708731e7bac50b9d1bcd2f3
Dominik Jambrović <dominik.jambrovic@gmail.com>
2023-05-30 14:37:27 +0200

    comments in headers

--- include/RIndex.hpp
+++ include/RIndex.hpp
@@ -7,27 +7,54 @@
 
 class RIndex {
 private:
+    // wavelet tree build upon BWT of original text
     Wavelet wavelet_tree;
+    // map containing sampled offsets in BWT and original text for each character
     map<char, map<int, int>> run_offsets;
+    // vector containing locations of left neighbours in suffix array of phrase starts
     vector<int> neighbours;
+    // bit vector for marking the starts of phrases in the original text
     bit_vector phrase_starts;
+    // rank support for phrase_starts bit vector and pattern '1'
     rank_support_v<1, 1> phrase_rank1;
 
 public:
+    // Constructor for R index
+    // params: input -- text upon which the R index will be built 
     explicit RIndex(const string &input);
 
+    // Constructor of helper structs needed for locate function
+    // params: str -- original text upon which the R index is built
+    //         sa -- suffix array of original text
     void build_locate_structs(const string &str, const vector<int> &sa);
 
+    // Function for finding the offset of previous occurence for given offset of some pattern
+    // params: k -- given offset of some pattern
+    // returns: offset of previous occurence of the same pattern
     int find_neighbours_offset(int k) const;
 
+    // Function for finding the offset in BWT and in the original text of character in BWT preceding lexicographically largest suffix (search limited by given offset)
+    // params: c -- character in BWT
+    //         offset -- offset limiting the search area in BWT
     pair<int, int> pred(char c, int offset) const;
 
+    // Match helper function using R index
+    // params: pattern -- pattern used for querying the R index
+    // returns: upper and lower bounds of the SA interval (bottom exclusive), offset of some occurance of the pattern in BWT and in the original text
     tuple<int, int, int, int> match(const string &pattern) const;
 
+    // Count function using R index
+    // params: pattern -- pattern used for querying the R index
+    // returns: number of occurences of the pattern in the original text
     int count(const string &pattern) const;
 
+    // Locate function using R index
+    // params: pattern -- pattern used for querying the R index
+    // returns: vector of sorted offsets of the pattern occurences in the original text
     vector<int> locate(const string &pattern) const;
 
+    // Function for printing the sorted offsets of the pattern occurences in the original text
+    // params: pattern -- pattern used for querying the R index
     void print_pattern_offsets(const string &pattern) const;
 };
 
--- include/Wavelet.hpp
+++ include/Wavelet.hpp
@@ -11,18 +11,30 @@ using namespace std;
 using namespace sdsl;
 
 struct Node {
+    // pointer to parent node
     weak_ptr<Node> parent;
+    // pointer to left child node
     shared_ptr<Node> left = nullptr;
+    // pointer to right child node
     shared_ptr<Node> right = nullptr;
 
+    // bit vector representing the current string
     bit_vector b_vector;
+    // rank support for current string bit vector and pattern '0'
     rank_support_v<0, 1> rank0;
+    // rank support for current string bit vector and pattern '1'
     rank_support_v<1, 1> rank1;
+    // select support for current string bit vector and pattern '0'
     select_support_mcl<0, 1> select0;
+    // select support for current string bit vector and pattern '1'
     select_support_mcl<1, 1> select1;
 
+    // character of leaf node
     char chr = 0;
 
+    // Constructor for Node
+    // params: str --  current string for which the node bit vector will be built
+    //         right -- characters which will be represented with a 1 in the bit vector
     Node(const string &str, const vector<char> &right) {
         unordered_set<char> right_character_set(right.begin(), right.end());
         b_vector = bit_vector(str.size(), 0);
@@ -37,6 +49,8 @@ struct Node {
         this->select1 = select_support_mcl<1, 1>(&(this->b_vector));
     };
 
+    // Constructor for Node (Leaf)
+    // params: c -- character of leaf node
     explicit Node(char c) {
         chr = c;
     }
@@ -44,15 +58,28 @@ struct Node {
 
 class Wavelet {
 private:
+    // pointer to root node
     shared_ptr<Node> start;
+    // map containing offsets in SA for each unique character
     map<char, int> char_offsets;
+    // map containing counts of each unique character
     map<char, int> char_counts;
+    // map containing labels for each unique character (e.g. '00')
     unordered_map<char, string> labels;
+    // total length of text
     int text_len = 0;
 
 private:
+    // Function for recursive building of the tree
+    // params: str -- current string for which the node bit vector will be built
+    //         alphas -- sorted characters contained in string
+    //         label -- current node level label 
+    // returns: pointer to built node
     shared_ptr<Node> build_tree(const string &str, const vector<char> &alphas, const string &label = "");
 
+    // Function for splitting the current alphabet into two sets (split roughly into halves)
+    // params: alphas -- sorted characters contained in some string
+    // returns: first set of characters, second set of characters
     static tuple<vector<char>, vector<char>> get_alphabets(const vector<char> &alphas) {
         vector<char> left;
         vector<char> right;
@@ -68,24 +95,50 @@ private:
     }
 
 public:
+    // Default constructor for wavelet tree
     explicit Wavelet() = default;
 
+    // Constructor for wavelet tree
+    // params: str -- string upon which the wavelet tree will be built
     explicit Wavelet(const string &str);
 
+    // Getter for root node
+    // returns: pointer to root node
     shared_ptr<Node> get_start() const;
 
+    // Getter for text length of the string upon which the wavelet tree was built
+    // returns: text length
     int get_text_len() const;
 
+    // Getter for char offset in the SA for some character
+    // params: c -- character for which the offset is needed
+    // returns: offset of the character c in the SA
     int get_char_offset(char c) const;
 
+    // Function for calculating the offset in BWT after applying LF mapping
+    // params: c -- character for which LF mapping is used
+    //         index -- current offset in BWT (before applying LF mapping)
+    // returns: offset of character after applying LF mapping
     int lf_mapping(char c, int index) const;
 
+    // Function for finding the character at some index 
+    // params: index -- offset in original string
+    // returns: character at given index in original string
     char access(unsigned long index) const;
 
+    // Function for calculating the rank of a given character at some index
+    // params: character -- character for which rank is calculated
+    //         index -- offset of character for which rank is calculated
+    // returns: calculated rank operation result
     int rank(char character, int index) const;
 
+    // Function for calculating the select operation of a given character and given rank
+    // params: character -- character for which select is calculated
+    //         index -- rank of character for which select is calculated
+    // returns: calculated select operation result
     int select(char character, int index) const;
 
+    // Function for printing out the wavelet tree structure
     void print() const;
 };
 
